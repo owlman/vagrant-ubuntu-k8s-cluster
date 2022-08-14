edca3621ab803f2434a0dd53399a9f08dbebdaae
owlman <jie.owl2008@gmail.com>
2022-08-14 14:10:00 +0800

    添加中文注释

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,27 +1,29 @@
 #! /bin/bash
 
+# 指定要安装哪一个版本的K8s
 KUBERNETES_VERSION="1.21.1-00"
 
-# disable swap 
+# 关闭swap分区
 sudo swapoff -a
 sudo sed -ri 's/.*swap.*/#&/' /etc/fstab 
 
 echo "Swap diasbled..."
 
-# disable firewall
+# 关闭防火墙功能
 sudo ufw disable
 
-# install dependencies
-sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl wget software-properties-common
-curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
+# 安装一些 Docker+k8s 环境的依赖项
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl wget software-properties-common
 
 echo "Dependencies installed..."
 
-# configure docker
+# 安装并配置 Docker CE
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install -y docker-ce
+
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "registry-mirrors": ["https://registry.cn-hangzhou.aliyuncs.com"],
@@ -29,23 +31,27 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
-# start docker
+# 启动 Docker
 sudo systemctl enable docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 echo "Docker installed and configured..."
 
-# install kubelet, kubectl, kubeadm
+# 安装 k8s 组件：kubelet, kubectl, kubeadm
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubenetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
+sudo apt update -y
+sudo apt install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION
 
-sudo apt-get update -y
-sudo apt-get install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION
+# 如果想阻止自动更新，可以选择锁住相关软件的版本
+sudo apt-mark hold kubeadm kubectl kubelet
 
+# 启动 K8s 的服务组件：kubelet
 sudo systemctl start kubelet  
 sudo systemctl enable kubelet   
 
-echo "Installation done..."
+echo "K8s installed and configured..."
+
--- scripts/master.sh
+++ scripts/master.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
+# 指定主控节点的IP地址
 MASTER_IP="192.168.100.21"
+# 指定主控节点的主机名
 NODENAME=$(hostname -s)
+# 指定当前 K8s 集群中 Service 所使用的 CIDR
 SERVICE_CIDR="10.96.0.0/12"
+# 指定当前 K8s 集群中 Pod 所使用的 CIDR
 POD_CIDR="10.244.0.0/16"
+# 指定当前使用的 K8s 版本
 KUBE_VERSION=v1.21.1
 
-# preload coredns for special handling
+# 特别预先加载 coredns 插件
 COREDNS_VERSION=1.8.0
 sudo docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$COREDNS_VERSION
 sudo docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$COREDNS_VERSION registry.cn-hangzhou.aliyuncs.com/google_containers/coredns/coredns:v$COREDNS_VERSION
 
-# kubeadm init
+# 使用 kubeadm 工具初始化 K8s 集群
 sudo kubeadm init \
   --kubernetes-version=$KUBE_VERSION \
   --apiserver-advertise-address=$MASTER_IP \
@@ -21,11 +26,12 @@ sudo kubeadm init \
   --node-name=$NODENAME \
   --ignore-preflight-errors=Swap
 
+# 生成主控节点的配置文件
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# save configs
+# 将主控节点的配置文件备份到别处
 config_path="/vagrant/configs"
 
 if [ -d $config_path ]; then
@@ -38,12 +44,14 @@ sudo cp -i /etc/kubernetes/admin.conf $config_path/config
 sudo touch $config_path/join.sh
 sudo chmod +x $config_path/join.sh       
 
+# 将往 K8s 集群中添加工作节点的命令保存为脚本文件
 kubeadm token create --print-join-command > $config_path/join.sh
 
-# install calico network plugin
+# 安装名为 calico 的网路插件
 sudo wget https://docs.projectcalico.org/manifests/calico.yaml
 sudo kubectl apply -f calico.yaml
 
+# 针对 Vagrant+VirtualBox 虚拟机环境的一些特定处理
 sudo -i -u vagrant bash << EOF
 mkdir -p /home/vagrant/.kube
 sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
